5dd358ba1b1997703080d6bc0b5e0367e0fc44dc
otol <satriotol69@gmail.com>
2021-03-10 03:15:24 +0700

    index

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -30,7 +30,33 @@
             <div class="row">
                 <div class="col-lg-3 col-6">
                     <!-- small box -->
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>{{$kategoris}}</h3>
+                            <p>Kategori</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
+                        <a href="{{route('kategori.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
                     <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>{{$kategoris}}</h3>
+                            <p>Kategori</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
+                        <a href="{{route('kategori.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-warning">
                         <div class="inner">
                             <h3>{{$kategoris}}</h3>
                             <p>Kategori</p>
