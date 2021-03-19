7e92785da3d7432a7c9d05a827fe216906df68ac
anggabaeee <53551376+anggabaeee@users.noreply.github.com>
2021-03-19 22:04:33 +0700

    responsive

--- resources/views/page/detail_lomba.blade.php
+++ resources/views/page/detail_lomba.blade.php
@@ -2,10 +2,10 @@
 @section('content')
 <!-- ======= Tentang Section ======= -->
 <main id="main">
-<section id="about" class="about mt-5">
+    <section id="about" class="about mt-5">
         <div class="container" data-aos="fade-up">
             <div class="row mt-1 mb-4">
-                <div class="col-lg-12">
+                <div class="col-md-12">
                     <span style="color:#cda45e;">{{$lomba->name}} - {{$lomba->kategori->name}} -
                         @foreach($lomba->jenjang as $j)
                         {{$j->name}}
@@ -15,11 +15,15 @@
             </div>
         </div>
         <div class="container detail">
-            <div class="detail-lomba">
-                <img style="width:100%; height:300px; position: relative; object-fit: cover;"
-                    src="{{asset('storage/'.$lomba->image)}}">
+            <div class="row mt-5">
+                <div class="col-md-8 offset-md-4">
+                    <div class="detail-lomba">
+                        <img width="400px" height="400px" style="object-fit: cover;"
+                            src="{{asset('storage/'.$lomba->image)}}">
+                    </div>
+                </div>
             </div>
-            <div class="row">
+            <div class="row mt-5 mb-4">
                 <div class="col-md-12">
                     <p class="font-italic">
                         Detail lomba
@@ -33,15 +37,14 @@
                         class="btn btn-secondary" target="_blank">Ikuti Lomba</a>
                 </div>
             </div>
-        </div>
-        <div class="container mt-5">
-            <div class="row">
-                <div class="col-md-6 offset-1">
-                    <img src="media.jpg" width="800px" alt="" srcset="">
+            <div class="row mt-5">
+                <div class="video-lomba col-md-6 offset-md-3">
+                    <video width="600px" class="mx-auto" height="400px" controls>
+                        <source src="" type="video/mp4">
                 </div>
             </div>
         </div>
-</section><!-- End About Section -->
+    </section><!-- End About Section -->
 </main><!-- End #main -->
 
 @endsection
