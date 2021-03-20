5306d177a589eb3c3955302078d4888da5d3251e
anggabaeee <53551376+anggabaeee@users.noreply.github.com>
2021-03-20 12:43:34 +0700

    clear UI

--- resources/views/page/detail_lomba.blade.php
+++ resources/views/page/detail_lomba.blade.php
@@ -6,11 +6,11 @@
         <div class="container" data-aos="fade-up">
             <div class="row mt-1 mb-4">
                 <div class="col-md-12">
-                    <span style="color:#cda45e;">{{$lomba->name}} - {{$lomba->kategori->name}} -
+                    <p style="font-weight:bold; color:black;">{{$lomba->name}} - {{$lomba->kategori->name}} -
                         @foreach($lomba->jenjang as $j)
                         {{$j->name}}
                         @endforeach
-                    </span>
+                    </p>
                 </div>
             </div>
         </div>
@@ -35,10 +35,9 @@
                     <p>{{$lomba->description}}
                     </p>
 
-                    <p class="mb-3" style="color:#cda45e;"> Deadline : {{$lomba->start_date}} - {{$lomba->end_date}}
+                    <p class="mb-3" style="font-weight:bold; color:black;"> Deadline : {{$lomba->start_date}} - {{$lomba->end_date}}
                     </p>
-                    <a href="{{$lomba->link}}" style=" border-radius: 30px; padding: 10px; background-color: #cda45e;"
-                        class="btn btn-secondary" target="_blank">Ikuti Lomba</a>
+                    <a href="{{$lomba->link}}" style="color:black;"class="BtnOrange" target="_blank">Ikuti Lomba</a>
                 </div>
             </div>
             <section id="" class="section-bg">
