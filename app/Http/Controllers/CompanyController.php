284f51f64c6a7e0858eb524b4b4630a523906032
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2021-04-06 21:36:02 +0700

    merapikan tok bgst

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -13,47 +13,47 @@ class CompanyController extends Controller
 {
     public function editmenu(Companys $company)
     {
-        return view('admin.company.menu_utama')->with('company',$company);
+        return view('admin.company.menu_utama')->with('company', $company);
     }
 
     public function updatemenu(UpdateCompanyMenuRequest $request, Companys $company)
     {
-        $data = $request->only(['name','email','address','phone_number']);
+        $data = $request->only(['name', 'email', 'address', 'phone_number']);
         $company->update($data);
-        session()->flash('success','Company Updated Successfully');
-        return redirect(route('company.menu',1));
+        session()->flash('success', 'Company Updated Successfully');
+        return redirect(route('company.menu', 1));
     }
     public function editberanda(Companys $company)
     {
-        return view('admin.company.menu_beranda')->with('company',$company);
+        return view('admin.company.menu_beranda')->with('company', $company);
     }
     public function updateberanda(UpdateCompanyBerandaRequest $request, Companys $company)
     {
-        $data = $request->only(['description_singkat','video_profile']);
+        $data = $request->only(['description_singkat', 'video_profile']);
         $company->update($data);
-        session()->flash('success','Company Updated Successfully');
-        return redirect(route('company.beranda',1));
+        session()->flash('success', 'Company Updated Successfully');
+        return redirect(route('company.beranda', 1));
     }
     public function edittentang(Companys $company)
     {
-        return view('admin.company.menu_tentang')->with('company',$company);
+        return view('admin.company.menu_tentang')->with('company', $company);
     }
     public function updatetentang(UpdateCompanyTentangRequest $request, Companys $company)
     {
-        $data = $request->only(['description','apa_kata_mereka','vision','mission']);
+        $data = $request->only(['description', 'apa_kata_mereka', 'vision', 'mission']);
         $company->update($data);
-        session()->flash('success','Company Updated Successfully');
-        return redirect(route('company.tentang',1));
+        session()->flash('success', 'Company Updated Successfully');
+        return redirect(route('company.tentang', 1));
     }
     public function editsosialmedia(Companys $company)
     {
-        return view('admin.company.menu_sosialmedia')->with('company',$company);
+        return view('admin.company.menu_sosialmedia')->with('company', $company);
     }
     public function updatesosialmedia(UpdateCompanyBerandaRequest $request, Companys $company)
     {
-        $data = $request->only(['description_singkat','video_profile']);
+        $data = $request->only(['description_singkat', 'video_profile']);
         $company->update($data);
-        session()->flash('success','Company Updated Successfully');
-        return redirect(route('company.sosialmedia',1));
+        session()->flash('success', 'Company Updated Successfully');
+        return redirect(route('company.sosialmedia', 1));
     }
 }
