0edd3ebfeeddadd8e15e8127975e8d577fd25991
satriotolinux <satriotol69@gmail.com>
2021-04-25 13:52:03 +0700

    revisi otol udah kelar asw

--- resources/views/page/faq.blade.php
+++ resources/views/page/faq.blade.php
@@ -1,17 +1,49 @@
 @extends('layouts.main')
 @section('content')
+<style>
+    h1.position-relative {
+        top: 40%;
+    }
+
+    @media (max-width: 767.98px) {
+        h1 {
+            font-size: 1.6rem
+        }
+
+        h1.position-relative {
+            top: 0%;
+        }
+
+        h3 {
+            font-size: 1.4rem
+        }
+
+        p {
+            font-size: 1.2rem
+        }
+    }
+
+</style>
 <section class="bg-utama margin-nav overflow-hidden">
     <div class="container text-center">
         <div class="row">
-            <div class="col-md-12 mt-5 mb-5 ">
-                <h1 class="txt-dark-blue mt-5" data-aos="fade-left" data-aos-duration="1000" data-aos-once="true"></h1>
-                <h1 class="txt-dark-blue " data-aos="fade-right" data-aos-duration="1000" data-aos-once="true"> <span class="border-blue">
-                        Frequently Asked Question</span></h1>
-                @foreach ($faqs as $faq)
-                <h3 class="txt-dark-blue deskripsi mt-4" data-aos="fade-left" data-aos-duration="1000" data-aos-once="true">{{$faq->pertanyaan}}</h3>
-                <P class="deskripsi txt-dark-blue mt-2" data-aos="fade-right" data-aos-duration="1000" data-aos-once="true">Lorem Ipsum is
-                    {{$faq->jawaban}}
-                </P>
+            <div class="col-md-12 my-5">
+                <h1 class="txt-dark-blue " data-aos="fade-right" data-aos-duration="1000" data-aos-once="true">
+                    Frequently Asked Question</h1>
+                @foreach ($faqs as $index => $faq)
+                <div class="row txt-dark-blue deskripsi mt-4">
+                    <div class="col-sm-1 text-center">
+                        <h1 class="position-relative">
+                            {{$index +1}}
+                        </h1>
+                    </div>
+                    <div class="col-sm-11">
+                        <h3 data-aos="fade-left" data-aos-duration="1000" data-aos-once="true">{{$faq->pertanyaan}}</h3>
+                        <p data-aos="fade-right" data-aos-duration="1000" data-aos-once="true" class="my-0">
+                            {{$faq->jawaban}}
+                        </p>
+                    </div>
+                </div>
                 @endforeach
             </div>
         </div>
