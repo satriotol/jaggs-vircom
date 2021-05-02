58a23b21ecfc9fc7dbdf6a45e9389ad6fb8dce95
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2021-05-03 00:52:31 +0700

    header clear

--- resources/views/page/beranda.blade.php
+++ resources/views/page/beranda.blade.php
@@ -1,18 +1,20 @@
 @extends('layouts.main')
 @section('content')
-<section class="bg-utama margin-nav overflow-hidden" >
-    <div class="container mt-4 ">
-        <div class="row welcome">
-            <div class="col-md-6">
-                <img src="{{asset('asset/home.png')}} " data-aos="fade-right" data-aos-once="true" data-aos-duration="1000"
-                    class="img-fluid img-welcome" alt="Responsive image">
-            </div>
-            <div class="col-md-5 offset-md-1">
-                <div class="text-welcome mb-5" data-aos="fade-left"  data-aos-once="true" data-aos-duration="1000">
-                    <h1 class="diplay-1">Young <span>Star</span></h1>
-                    <h1 class="diplay-3">Championship</h1>
-                    <a href="{{route('infolomba')}}"><button class="btn-master mt-2 mb-2">Info Lomba</button></a>
-                    <a href="https://linktr.ee/youngstarchampionship"><button class="btn-master">Registrasi</button></a>
+<section class="bg-utama margin-nav overflow-hidden">
+    <div class="mt-4 bg-header">
+        <div class="container">
+            <div class="row welcome">
+                <div class="col-md-5 offset-md-7">
+                    <div class="text-welcome" data-aos="fade-left" data-aos-once="true" data-aos-duration="1000">
+                        <h1 class="diplay-1 text-center">Young <span>Star</span></h1>
+                        <h1 class="diplay-3 font-weight-bold text-center">Championship</h1>
+                        <div class="text-center">
+                            <a href="{{route('infolomba')}}"><button class="btn-master mt-2 mb-2">Info
+                                    Lomba</button></a>
+                            <a href="https://linktr.ee/youngstarchampionship"><button
+                                    class="btn-master">Registrasi</button></a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -20,11 +22,11 @@
     <div class="container-fluid text-center bg-dua">
         <div class="row">
             <div class="col-md-12 mt-5 mb-5 ">
-                <h1 data-aos="fade-right" data-aos-duration="1000" data-aos-once="true" class="txt-dark-blue judul">Video Profil Young Star Championship</h1>
+                <h1 data-aos="fade-right" data-aos-duration="1000" data-aos-once="true" class="txt-dark-blue judul">
+                    Video Profil Young Star Championship</h1>
                 <div data-aos="fade-left" data-aos-duration="1000" data-aos-once="true"
                     class="mt-5 embed-responsive embed-responsive-16by9 mx-auto">
-                    <iframe class="embed-responsive-item" src="{{$company->video_profile}}"
-                        allowfullscreen></iframe>
+                    <iframe class="embed-responsive-item" src="{{$company->video_profile}}" allowfullscreen></iframe>
                 </div>
             </div>
         </div>
@@ -32,9 +34,11 @@
     <div class="container text-center mt-5 ">
         <div class="row">
             <div class="col-md-12 ">
-                <h1 class="txt-dark-blue judul text-capitalize" data-aos="fade-down" data-aos-once="true" data-aos-duration="1000">Apa Itu Young Star Championship?</h1>
+                <h1 class="txt-dark-blue judul text-capitalize" data-aos="fade-down" data-aos-once="true"
+                    data-aos-duration="1000">Apa Itu Young Star Championship?</h1>
                 <h4 class="txt-grey subjudul" data-aos="fade-down" data-aos-duration="1000">Penjelasan Singkat</h4>
-                <div class="mt-4 deskripsi txt-dark-blue" data-aos="fade-up" data-aos-duration="1000"><p class="text-center"> {!!$company->description_beranda!!}</p>
+                <div class="mt-4 deskripsi txt-dark-blue" data-aos="fade-up" data-aos-duration="1000">
+                    <p class="text-center"> {!!$company->description_beranda!!}</p>
                 </div>
 
                 <a href="{{route('tentang')}}" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true"><button
@@ -49,12 +53,12 @@
         </div>
         <div class="row ">
             <div class="col-md-6">
-                <img src="{{asset('asset/tatacara.png')}}" data-aos="fade-right" data-aos-duration="1000" data-aos-once="true"
-                    class="img-fluid img-welcome mb-5" alt="Responsive image">
+                <img src="{{asset('asset/tatacara.png')}}" data-aos="fade-right" data-aos-duration="1000"
+                    data-aos-once="true" class="img-fluid img-welcome mb-5" alt="Responsive image">
             </div>
             <div class="col-md-6" data-aos="fade-left" data-aos-duration="1000" data-aos-once="true">
                 @php
-                    $i = 1;
+                $i = 1;
                 @endphp
                 @foreach ($tatacaras as $tatacara)
                 <div class="row bubble mt-2 mr-2 ml-2 mt-2">
